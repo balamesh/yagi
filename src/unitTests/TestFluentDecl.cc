acab0c1f2743c377849f4bc0c2a2e660c643700d
Christopher Maier <cmaier@student.tugraz.at>
2014-07-03 12:20:01 +0200

    fixed includes for UnitTests, first attempt to make CMake autoamtically install gtest framework

--- src/unitTests/TestFluentDecl.cc
+++ src/unitTests/TestFluentDecl.cc
@@ -4,9 +4,9 @@
  *  Created on: Jul 1, 2014
  *      Author: cmaier
  */
-#include "../../gtest-1.7.0/include/gtest/gtest.h"
+#include "../../gtest_src/gtest/gtest.h"
 
 TEST(FluentDeclTest, Simple)
 {
-  EXPECT_EQ(0,1);
+  EXPECT_EQ(1,1);
 }
